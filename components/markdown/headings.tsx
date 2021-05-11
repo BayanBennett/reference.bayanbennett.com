79c85c4736c7c90e72c3da5878cc626cbfe1cdad
Bayan Bennett <bayan.bennett@gmail.com>
2021-05-11 13:26:15 -0700

    fix(markdown): only pass children to headings

--- components/markdown/headings.tsx
+++ components/markdown/headings.tsx
@@ -2,26 +2,28 @@ import { HeadingComponent } from "react-markdown/src/ast-to-react";
 import { Typography } from "@material-ui/core";
 import React from "react";
 
-export const h1: HeadingComponent = ({ ...props }) => (
-  <Typography {...props} variant="h1" align="center" />
+export const h1: HeadingComponent = ({ children }) => (
+  <Typography variant="h1" align="center">
+    {children}
+  </Typography>
 );
 
-export const h2: HeadingComponent = ({ ...props }) => (
-  <Typography {...props} variant="h2" />
+export const h2: HeadingComponent = ({ children }) => (
+  <Typography variant="h2">{children}</Typography>
 );
 
-export const h3: HeadingComponent = ({ ...props }) => (
-  <Typography {...props} variant="h3" />
+export const h3: HeadingComponent = ({ children }) => (
+  <Typography variant="h3">{children}</Typography>
 );
 
-export const h4: HeadingComponent = ({ ...props }) => (
-  <Typography {...props} variant="h4" />
+export const h4: HeadingComponent = ({ children }) => (
+  <Typography variant="h4">{children}</Typography>
 );
 
-export const h5: HeadingComponent = ({ ...props }) => (
-  <Typography {...props} variant="h5" />
+export const h5: HeadingComponent = ({ children }) => (
+  <Typography variant="h5">{children}</Typography>
 );
 
-export const h6: HeadingComponent = ({ ...props }) => (
-  <Typography {...props} variant="h6" />
+export const h6: HeadingComponent = ({ children }) => (
+  <Typography variant="h6">{children}</Typography>
 );
