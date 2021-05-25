9d4a255dc1d0d5c47ab8e865435268f4029c2877
Bayan Bennett <bayan.bennett@gmail.com>
2021-05-25 16:09:13 -0700

    feat(layout): add reference icon

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,17 @@
 import React, { FunctionComponent } from "react";
-import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Avatar,
+  Container,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 
 export const Layout: FunctionComponent = ({ children }) => (
   <>
     <AppBar position="sticky">
       <Toolbar>
+        <Avatar>📚</Avatar>
         <Typography variant="h6">Reference</Typography>
       </Toolbar>
     </AppBar>
