c7c73100e0dcfffe7bcf03462779780dd5ad7581
Bayan Bennett <bayan.bennett@gmail.com>
2021-05-31 23:25:52 -0700

    feat(home-page): added temp message so page isn't empty

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { IconJavaScript } from "../components/icons/javascript";
+import { Typography } from "@material-ui/core";
 
 const IndexPage = () => (
   <>
-    <IconJavaScript fontSize="large" />
+    <Typography align="center" variant="h1">
+      👷‍♂️
+    </Typography>
+    <Typography align="center" variant="h1">
+      I'm working on this!
+    </Typography>
+    <Typography align="center" variant="h2">
+      Please come back later
+    </Typography>
   </>
 );
 
